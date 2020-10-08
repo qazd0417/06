728fca3633516667649a4883860da8963c2aa966
qazd0417 <70630107+qazd0417@users.noreply.github.com>
2020-10-08 16:42:49 +0900

    ysj06_04

--- main.c
+++ main.c
@@ -3,15 +3,42 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
+int combination(int n, int r)
 {
-	return(a*a);
+	int c;
+	
+	c = factorial(n) / ((factorial(n-r))*(factorial(r)));
+	return c;
 }
 
+int factorial(int a)
+{
+	int res=1;
+	int i;
+	
+	for(i=1; i<=a; i++)
+		res *= i;
+		
+	return res;
+}
+
+int get_integer()
+{
+	int a;
+	
+	printf("숫자를 입력해주세요: ");
+	scanf("%i",&a);
+	
+	return a;
+}
 int main(int argc, char *argv[]) {
 	
-	int a=2;
-	a=square(a);
-	printf("a=%d\n", a);
+	int a, b, result;
+	
+	a = get_integer();
+	b = get_integer();
+	result=combination(a,b);
+	printf("combination result is %i\n", result);
+	
 	return 0;
 }
