7e18a4d9fa486fb24d5c7a01ec6de7e2436028b9
qazd0417 <70630107+qazd0417@users.noreply.github.com>
2020-10-08 15:50:30 +0900

    ysj06_02

--- main.c
+++ main.c
@@ -2,19 +2,31 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int starprint()
+int sumTwo(int a,int b)
 {
-	int i;
 	
-	for (i=0;i<10;i++)
-		printf("*");
+	return a+b;
+}
+
+int square(int n)
+{
+	return n*n;
+}
+int get_max(int x,int y)
+{
+	if (x>y)
+		return x;
+	else if(x<y)
+		return y;
+		
 }
 
 int main(int argc, char *argv[]) {
 	
-	starprint();
-	starprint();
-	starprint();
+
+	printf("sumTwo: %d\n",sumTwo(2,5) );
+	printf("square: %d\n",square(5));
+	printf("get_max: %d",get_max(5,7));
 	
 	return 0;
 }
